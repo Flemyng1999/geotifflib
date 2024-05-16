b5a485a2a2af8ae70b59c6c4d96f671b8ca158c1
Yangming Feng <flemyng1999@outlook.com>
2024-05-16 11:20:41 +0800

    Change python_requires

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     install_requires=[
         'GDAL',
         'numpy',
